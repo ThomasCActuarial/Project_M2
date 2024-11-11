b16e9ee5141621344b3bc74a3d9cc1a697764687
ThomasCActuarial <yackofdoomv2@gmail.com>
2024-11-11 16:42:18 +0100

    Aide

--- DataVIz/Projet_SWI.py
+++ DataVIz/Projet_SWI.py
@@ -22,13 +22,13 @@ df1 = pd.read_csv('data\MENS_SIM2_1980-1989.csv',delimiter=';')
 data=pd.concat([df1,df2,df3,df4,df5] ,ignore_index=True)
 
 data['DATE'] = pd.to_datetime(data['DATE'], format='%Y%m')
-data=data.sort_values(by=['KEY', 'DATE']).reset_index(drop=True)
+
 
 
 data['SPEI_1'] = data['PRELIQ_MENS']-data['ETP_MENS']
 
 data["KEY"] = data['LAMBY'].apply(str).str.cat( data['LAMBX'].apply(str))
-
+data=data.sort_values(by=['KEY', 'DATE']).reset_index(drop=True)
 #%%
 
 grouped_data = data.groupby(['KEY'])
@@ -42,21 +42,30 @@ grouped_data = data.groupby(['KEY'])
 #%%
 
 dataTest = data[ data["DATE"] !=datetime(2024,10,1,00,00,00)]
+TRUE = data[ data["DATE"] == datetime(2024,10,1,00,00,00) ][['SSWI12_MENS']]
 
 
 def two_largest_last_300(group):
-    # Vérifier qu'il y a au moins 300 observations
-    if len(group) >= 300:
         # Extraire les 300 dernières valeurs
         last_300_values = group['SSWI12_MENS'].iloc[-300:]
-        # Trouver les 2 plus grandes valeurs
-        top_2 = last_300_values.nlargest(2)
+        # revoie la 2ème plus grande valeur
+        top_2 = last_300_values.nlargest(2).reset_index()
+        top_2 = min(top_2["SSWI12_MENS"][1],top_2["SSWI12_MENS"][0])
+        
         return top_2
-    else:
-        return pd.Series([np.nan, np.nan], index=['top_1', 'top_2'])
+    
 
-# Appliquer la fonction à chaque groupe
 result = dataTest.groupby('KEY').apply(two_largest_last_300)
+
+
+
+#%%
+#tentative de modèle linaire 
+
+
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=2024)
+
+
 
         
         
