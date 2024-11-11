6fc80aac902d37ce1a45d64a1bb790e8058d2437
ThomasCActuarial <yackofdoomv2@gmail.com>
2024-11-11 14:39:51 +0100

    Update Projet_SWI.py

--- DataVIz/Projet_SWI.py
+++ DataVIz/Projet_SWI.py
@@ -22,3 +22,12 @@ data['DATE'] = pd.to_datetime(data['DATE'], format='%Y%m')
 
 data['SPEI_1'] = data['PRELIQ_MENS']-data['ETP_MENS']
 
+
+#regroupement des coordonnées par LAMBX et LAMBY
+grouped_data = data.groupby(['LAMBX','LAMBY']).sum().reset_index()
+grouped_data['bloc'] = ['BLOC_' + str(i+1)] for i in range(len(grouped_data))]
+
+
+#calcul de la moyenne glissante sur 3 mois
+
+data[Rolling_sum_SPEI] = data['SPEI1'].rolling(windows = 3).mean()
